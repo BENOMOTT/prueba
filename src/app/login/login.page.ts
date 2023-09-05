db451dfc4f0220414f5df7572dd01d9d7cec1fa6
Dylan Ramirez <dylan.r.ubilla@gmail.com>
2023-09-05 03:59:11 -0300

    validacion de login con contraseña con 4 numeros 3 caracteres y 1 mayus

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,29 +8,41 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class LoginPage {
   formularioLogin: FormGroup;
-  isModalOpen: boolean = false; // Agrega esta variable
+  isModalOpen: boolean = false;
 
   constructor(private router: Router, private formBuilder: FormBuilder) {
     this.formularioLogin = this.formBuilder.group({
       nombre: ['', Validators.required],
-      contraseña: ['', Validators.required],
+      contraseña: ['', [Validators.required, this.validaContraseña]], // Usar la función de validación personalizada
     });
   }
 
   iniciarSesion() {
     if (this.formularioLogin.valid) {
-      // Aquí puedes agregar más condiciones si es necesario
-
-      // Redirige a la página "home.page.html"
       this.router.navigate(['/home']);
     } else {
-      // Muestra un mensaje de error o realiza otra acción si no se cumplen las condiciones
-      console.log('Por favor, complete todos los campos.');
+      console.log('Por favor, complete todos los campos y asegúrese de que la contraseña cumpla con los requisitos.');
     }
   }
-
-  // Define el método setOpen para abrir y cerrar el modal
+  
   setOpen(open: boolean) {
     this.isModalOpen = open;
+  }
+  
+  validaContraseña(control: AbstractControl) {
+    const password = control.value;
+
+
+    const hasFourNumbers = (password.match(/\d/g) || []).length >= 4;
+
+    const hasThreeChars = (password.match(/[a-zA-Z]/g) || []).length >= 3;
+
+    const hasUpperCase = /[A-Z]/.test(password);
+
+    if (hasFourNumbers && hasThreeChars && hasUpperCase) {
+      return null; 
+    } else {
+      return { invalidPassword: true };
+    }
   }
 }
