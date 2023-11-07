4f285bd64c4116a70c22d81278588425466572ab
matiasvicencio <mat.vicencio@duocuc.cl>
2023-11-07 17:10:31 -0300

    canActivate en los path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -38,43 +38,53 @@ const routes: Routes = [
 
   {
     path: 'product-add',
-    loadChildren: () => import('./productos/product-add/product-add.module').then( m => m.ProductAddPageModule)
+    loadChildren: () => import('./productos/product-add/product-add.module').then( m => m.ProductAddPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'product-all',
     loadChildren: () => import('./productos/product-all/product-all.module').then( m => m.ProductAllPageModule)
+    ,canActivate: [AuthGuard]
   },
   {
     path: 'product-detail/:id',
     loadChildren: () => import('./productos/product-detail/product-detail.module').then( m => m.ProductDetailPageModule)
+    ,canActivate: [AuthGuard]
   },
   {
     path: 'product-edit/:id',
     loadChildren: () => import('./productos/product-edit/product-edit.module').then( m => m.ProductEditPageModule)
+    ,canActivate: [AuthGuard]
   },
   {
     path: 'product-list',
     loadChildren: () => import('./productos/product-list/product-list.module').then( m => m.ProductListPageModule)
+    ,canActivate: [AuthGuard]
   },
   {
     path: 'user-add',
     loadChildren: () => import('./usuarios/user-add/user-add.module').then( m => m.UserAddPageModule)
+    ,canActivate: [AuthGuard]
   },
   {
     path: 'user-all',
     loadChildren: () => import('./usuarios/user-all/user-all.module').then( m => m.UserAllPageModule)
+    ,canActivate: [AuthGuard]
   },
   {
     path: 'user-detail/:id',
     loadChildren: () => import('./usuarios/user-detail/user-detail.module').then( m => m.UserDetailPageModule)
+    ,canActivate: [AuthGuard]
   },
   {
     path: 'user-edit/:id',
     loadChildren: () => import('./usuarios/user-edit/user-edit.module').then( m => m.UserEditPageModule)
+    ,canActivate: [AuthGuard]
   },
   {
     path: 'user-list',
     loadChildren: () => import('./usuarios/user-list/user-list.module').then( m => m.UserListPageModule)
+    ,canActivate: [AuthGuard]
   },
   {
     path: 'camera',
